3dec222737ddfb5b6cc7f4f526459810e00388b3
AlexGlering <afglering@gmail.com>
2023-04-23 20:02:50 +0100

    Current stable build

--- weatherAPI.js
+++ weatherAPI.js
@@ -2,13 +2,13 @@ const axios = require("axios");
 
 // This function fetches weather data for a given city and number of days,
 // and optionally saves the fetched data to the server-side database
-const weatherAPI = (city, days, shouldSaveData = false) => {
+const weatherAPI = (city, days = false) => {
   // Configure the options for the API request
   const options = {
     method: "GET",
     url: "https://weatherapi-com.p.rapidapi.com/forecast.json",
-    // Pass city, days, and shouldSaveData as query parameters
-    params: { q: city, days: days, shouldSaveData },
+    // Pass city, days, and as query parameters
+    params: { q: city, days: days },
     headers: {
       // The API key and host are required for authenticating with the Weather API
       "X-RapidAPI-Key": "bf37078114mshf90b5103cee1f0dp176d3cjsn3b6c0adcfc58",
