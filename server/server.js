907038e541f9962d02a44a9687c063d4ee2a76f2
Kochemanden <olkoc21@student.sdu.dk>
2023-05-01 21:58:25 +0200

    Oliver solution works now
    Should be more optimized. I tried to fix indents, hopefully it is correct...

--- server/server.js
+++ server/server.js
@@ -33,39 +33,55 @@ app.post("/weather", async (req, res) => {
       },
     });
     */
-
+    console.log("Hi! I am alive");
 
     const currentDate = new Date().toISOString().split('T')[0];
-
-    const forecastsExpired = await Forecast.findAll({
+    
+    
+    // Checking if info on the city exists in the DB already
+    const inDatabase = await Forecast.findAll({
       where: {
         city: city,
-        date: {
-          [Op.lt]: currentDate,
-        },
       },
-    });
+    })
 
-    // Delete any existing entries for the city
-    if (forecastsExpired.length >= 1){
-      await Forecast.destroy({
-        where: {
-          city: city,
-        },
+    // If inDatabase is not empty then it exists in the DB if not we can just immediately insert the pulled information.
+    if (inDatabase.length > 0){
+    
+      // Checks if any of the forecasts for the given city is out of date.
+      const forecastsExpired = inDatabase.filter((forecast) => {
+        return forecast.date < currentDate;
       });
-    }
 
 
-    // Insert the new forecast data
-    const savedForecasts = await Forecast.bulkCreate(forecasts);
-
-    res.status(200).json(savedForecasts);
+    // Delete any existing entries for the city if anything is expired or data is missing
+      if (forecastsExpired.length >= 1 || inDatabase.length != 3){
+      
+        await Forecast.destroy({
+          where: {
+            city: city,
+          },
+        });
+        const savedForecasts = await Forecast.bulkCreate(forecasts);
+        res.status(200).json(savedForecasts);
+      } 
+      else{
+        // If the city exists in the DB and nothing is expired,then just present the info from the database.
+        res.status(200).json(inDatabase);
+      }
+    } else{ 
+        // The city is not in the DB
+        const savedForecasts = await Forecast.bulkCreate(forecasts);
+        res.status(200).json(savedForecasts);
+      }
   } catch (error) {
-    console.error("Error fetching weather data:", error);
-    res.status(500).json({ message: "Error fetching weather data" });
+      console.error("Error fetching weather data:", error);
+      res.status(500).json({ message: "Error fetching weather data" });
+    }
   }
-});
+);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
